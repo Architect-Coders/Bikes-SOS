8cdebd06e40c6df7081305d3139372f3f94edd4d
Rodrigo Dámazo <damazod.rb@gmail.com>
2019-12-17 20:13:41 -0600

    Implementing Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compileSdk)
+    buildToolsVersion(AndroidSdk.buildToolsVersion)
+    defaultConfig {
+        applicationId = "com.damazo.mountainbikes"
+        minSdkVersion(AndroidSdk.minSdk)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.ktxCore)
+    implementation(Libraries.constraintLayout)
+
+    testImplementation(TestLibraries.junit4)
+    androidTestImplementation(TestLibraries.extJunit)
+    androidTestImplementation(TestLibraries.espresso)
+}
